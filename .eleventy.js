e83d05e4b93e63eaf854b2977e8dd159b575b213
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-08-25 20:33:57 +0100

    implemented assets and plugins in .eleventy.js config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+const { EleventyRenderPlugin } = require('@11ty/eleventy')
+const syntaxHighlight  = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addWatchTarget("style");
+  eleventyConfig.addPassthroughCopy("src/assets/style.css")
+  eleventyConfig.addPlugin(EleventyRenderPlugin)
+  eleventyConfig.addPlugin(syntaxHighlight)
   return {
     dir: {
       input: "src",
